a614ac6086028e820a037e38cb056d94d5b301f1
FreakJoe <sandmensch933@web.de>
2017-02-04 11:08:47 +0100

    Extended document name validation

--- src/main/java/CryptoLock/Controller.java
+++ src/main/java/CryptoLock/Controller.java
@@ -9,9 +9,17 @@ import javafx.scene.control.TextField;
 public class Controller {
     @FXML TextField documentName;
 
+    /**
+     * Checks a document name for validity.
+     * A document name is valid if it starts with a character or a number,
+     * ends with a character or a number,
+     * doesn't contain special characters except hyphens and underscores.
+     */
     public static boolean isValidDocument(String document) {
         boolean valid = true;
         if (document.isEmpty()) {
+            valid = false;
+        } else if (!document.matches("^[A-Za-z0-9]{1}[A-Za-z0-9-_]*[A-Za-z0-9]{1}$")) {
             valid = false;
         }
 
@@ -21,10 +29,12 @@ public class Controller {
     @FXML protected void addDocument(ActionEvent event) {
         String document = documentName.getText();
         if (!isValidDocument(document)) {
-            Alert alert = new Alert(Alert.AlertType.NONE, "Please enter a valid document name.", ButtonType.OK);
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Please enter a valid document name.", ButtonType.OK);
+            alert.setHeaderText(null);
             alert.show();
         } else {
-            Alert alert = new Alert(Alert.AlertType.NONE, "Success.", ButtonType.OK);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Success.", ButtonType.OK);
+            alert.setHeaderText(null);
             alert.show();
         }
     }
